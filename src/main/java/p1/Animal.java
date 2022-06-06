8ba8171d362828538b9b2e2ce5db727864b0b909
wangyacong <wangyacong@corp.netease.com>
2022-06-06 17:09:51 +0800

    add animal

---
+++ src/main/java/p1/Animal.java
@@ -0,0 +1,17 @@
+/*
+ * @(#) Animal.java 2022-06-06
+ *
+ * Copyright 2022 NetEase.com, Inc. All rights reserved.
+ */
+
+package p1;
+
+/**
+ * @author wangyacong
+ * @version 2022-06-06
+ */
+public class Animal {
+    public void show() {
+        System.out.println("这是一个动物");
+    }
+}
